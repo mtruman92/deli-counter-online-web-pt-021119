421b763d62af68212ac53b48bb6436751dbfe23a
Mickae Truman <mtruman@nyit.edu>
2019-02-19 18:21:57 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,9 +1,12 @@
 katz_deli = []
 
-def line(y)
-  line.array = []
-  if y.length == 0 
-     puts "The line is currently empty."
-   else
-     puts "The line is currently"
-  
+def line(customer)
+line_array = []
+if customer.length == 0
+puts "The line is currently empty."
+else
+customer
+.each
+.with_index(1) do |customer,index|
+puts "#{index}) with #{customer}"
+end
