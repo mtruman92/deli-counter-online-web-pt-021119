59b283eb8051a8331287cdc1d10dfbd067195a83
Mickae Truman <mtruman@nyit.edu>
2019-02-19 17:56:50 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,22 +1,3 @@
-def take_a_number(katz_deli,name)
-  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
-end
-
-def now_serving([name,katz_deli])
-  if katz_deli.length == 0 
-    puts "There is nobody waiting to be served!"
-  else
-    puts "Currently serving #{katz_deli.length and name}."
-  end
-end
-
-def line(katz_deli)
-  if katz_deli.length == 0 
-    puts "The line is currently empty."
-  else
-    puts "The line is currently: #{name}"
-  end
-
 katz_deli = []
 
 def line(y)
