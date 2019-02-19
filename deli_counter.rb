0eeffe3662fa98e8d5bc651ce94978867d4a0a0c
Mickae Truman <mtruman@nyit.edu>
2019-02-19 19:23:57 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,4 +17,8 @@ def take_a_number(katz_deli, name)
 end
 def now_serving(katz_deli)
   if katz_deli.length == 0 
-    puts "T"
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{name}"
+  end
+end
